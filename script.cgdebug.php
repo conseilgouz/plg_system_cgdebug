1628f70d61cca8881cf2f3d16a2fa4ed7f2895b1
Pascal Leconte <pascal.leconte@conseilgouz.com>
2025-04-11 09:21:24 +0200

    1.1.3

--- script.cgdebug.php
+++ script.cgdebug.php
@@ -10,6 +10,7 @@ defined('_JEXEC') or die('Restricted access');
 use Joomla\CMS\Factory;
 use Joomla\Database\DatabaseInterface;
 use Joomla\Filesystem\File;
+use Joomla\Filesystem\Folder;
 
 class plgSystemcgdebugInstallerScript
 {
